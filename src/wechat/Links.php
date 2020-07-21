2151fe1343ed879d0afca30358f7d7f71535cd75
zwd <zhiweiding1978@163.com>
2020-07-21 16:33:59 +0800

    微信正扫支付方法

--- src/wechat/Links.php
+++ src/wechat/Links.php
@@ -7,6 +7,9 @@ class Links
     //统一下单
     const CREATE_ORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 
+    //扫码支付
+    const MICRO_PAY = "https://api.mch.weixin.qq.com/pay/micropay";
+
     //订单查询
     const ORDER_QUERY = "https://api.mch.weixin.qq.com/pay/orderquery";
 
---
+++ src/wechat/payment/apis/MicroPay.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace coldcolor\pay\wechat\payment\apis;
+
+use coldcolor\pay\Utils;
+use coldcolor\pay\wechat\Links;
+use coldcolor\pay\wechat\payment\PaymentRequest;
+
+/**
+ * 微信正扫支付
+ * Class MicroPay
+ * @package coldcolor\pay\wechat\payment\apis
+ */
+class MicroPay extends PaymentRequest
+{
+    protected $url = Links::MICRO_PAY;
+
+    public $body;
+
+    public $out_trade_no;
+
+    public $total_fee;
+
+    public $trade_type;
+
+    public $auth_code;
+
+    public $spbill_create_ip;
+
+    protected function setData()
+    {
+        $this->spbill_create_ip = Utils::getClientIP();
+
+        if (!empty($this->body))
+            $this->data['body'] = $this->body;
+
+        if (!empty($this->out_trade_no))
+            $this->data['out_trade_no'] = $this->out_trade_no;
+
+        if (!empty($this->total_fee))
+            $this->data['total_fee'] = $this->total_fee;
+
+        if (!empty($this->trade_type))
+            $this->data['trade_type'] = $this->trade_type;
+
+        if (!empty($this->auth_code))
+            $this->data['auth_code'] = $this->auth_code;
+
+        if (!empty($this->spbill_create_ip))
+            $this->data['spbill_create_ip'] = $this->spbill_create_ip;
+    }
+}
